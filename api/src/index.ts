033fbe841e99276cc8e460a682f7fecba2fbe126
Pierce Lauring <pclauring@gmail.com>
2022-10-18 09:04:30 -0700

    node api docker scaffolding

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,14 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { Pool } from "pg";
+
+const port = process.env.PORT;
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
