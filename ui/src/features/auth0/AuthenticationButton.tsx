3a2328e159c1473df268b29abdc380bfc02ba88f
Pierce Lauring <pclauring@gmail.com>
2022-11-15 17:46:50 -0800

    Update layout for auth

--- ui/src/features/auth0/AuthenticationButton.tsx
+++ ui/src/features/auth0/AuthenticationButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import LoginButton from "./LoginButton";
-import LogoutButton from "./LogoutButton";
+import LoginButton from "./LoginButton/LoginButton";
+import LogoutButton from "./LogoutButton/LogoutButton";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import { Loader } from "./Loader";
--- ui/src/features/layout/Layout.tsx
+++ ui/src/features/layout/Layout.tsx
@@ -1,68 +1,18 @@
-import { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Outlet } from "react-router-dom";
 import AuthenticationButton from "../auth0/AuthenticationButton";
 import styles from "./Layout.module.css";
 
-interface LinkItem {
-  active: boolean;
-  label: string;
-  to: string;
-}
-
 export default function Layout() {
-  const [layoutItems, setLayoutItems] = useState<LinkItem[]>([
-    { active: true, label: "Home", to: "/" },
-    { active: false, label: "Monster", to: "/monster" },
-    { active: false, label: "Profile", to: "/profile" },
-  ]);
+  const { user } = useAuth0();
 
-  function handleLinkClick(e: React.MouseEvent<HTMLElement>) {
-    const newLayoutItems = layoutItems.map((item) => {
-      return { ...item, active: e.currentTarget.innerHTML === item.label };
-    });
-    setLayoutItems(newLayoutItems);
-  }
   return (
     <div>
       {/* A "layout route" is a good place to put markup you want to
             share across all the pages on your site, like navigation. */}
       <nav>
-        <ul className={styles.ul}>
-          {layoutItems &&
-            layoutItems?.map((item) => {
-              return (
-                <li className={styles.li} key={item.label}>
-                  <Link
-                    onClick={handleLinkClick}
-                    className={
-                      styles.link + `${item.active ? " " + styles.active : ""}`
-                    }
-                    to={item.to}
-                  >
-                    {item.label}
-                  </Link>
-                </li>
-              );
-            })}
-          <li className={styles.li}>
-            <AuthenticationButton />
-          </li>
-          {/* <li className={styles.li}>
-            <Link onClick={handleLinkClick} className={styles.link} to="/">
-              Home
-            </Link>
-          </li>
-          <li className={styles.li}>
-            <Link className={styles.link} to="/monster">
-              Monster
-            </Link>
-          </li>
-          <li className={styles.li}>
-            <Link className={styles.link} to="/Profile">
-              Profile
-            </Link>
-          </li> */}
-        </ul>
+        {user && <div>{user.email}</div>}
+        <AuthenticationButton />
       </nav>
       {/* An <Outlet> renders whatever child route is currently active,
             so you can think about this <Outlet> as a placeholder for
